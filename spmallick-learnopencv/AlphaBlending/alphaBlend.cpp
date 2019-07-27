c3a449f3a485418b087d3780e1e2b636b9c3b3fe
plding <77676182@qq.com>
2019-07-27 11:27:38 +0800

    update

---
+++ spmallick-learnopencv/AlphaBlending/alphaBlend.cpp
@@ -0,0 +1,92 @@
+#include <opencv2/opencv.hpp>
+
+using namespace cv;
+using namespace std;
+
+// Alpha blending using multiply and add functions
+Mat &blend(Mat &alpha, Mat &foreground, Mat &background, Mat &outImage)
+{
+    Mat fore, back;
+    multiply(alpha, foreground, fore);
+    multiply(Scalar::all(1.0) - alpha, background, back);
+    add(fore, back, outImage);
+
+    return outImage;
+}
+
+// Alpha blending using direct pointer access
+Mat &alphaBlendDirectAccess(Mat &alpha, Mat &foreground, Mat &background, Mat &outImage)
+{
+    int numberOfPixels = foreground.rows * foreground.cols * foreground.channels();
+
+    float *fptr = reinterpret_cast<float *>(foreground.data);
+    float *bptr = reinterpret_cast<float *>(background.data);
+    float *aptr = reinterpret_cast<float *>(alpha.data);
+    float *outImagePtr = reinterpret_cast<float *>(outImage.data);
+
+    for (int i = 0; i < numberOfPixels; ++i, ++outImagePtr, ++fptr, ++aptr, ++bptr) {
+        *outImagePtr = (*fptr) * (*aptr) + (*bptr) * (1 - *aptr);
+    }
+
+    return outImage;
+}
+
+int main()
+{
+    // Read in the png foreground asset file that contains both rgb and alpha information
+    Mat foreGroundImage = imread("foreGroundAssetLarge.png", -1);
+    Mat bgra[4];
+    split(foreGroundImage, bgra);
+
+    // Save the foreground RGB content into a single Mat
+    vector<Mat> foregroundChannels;
+    foregroundChannels.push_back(bgra[0]);
+    foregroundChannels.push_back(bgra[1]);
+    foregroundChannels.push_back(bgra[2]);
+    Mat foreground = Mat::zeros(foreGroundImage.size(), CV_8UC3);
+    merge(foregroundChannels, foreground);
+
+    // Save the alpha information into a single Mat
+    vector<Mat> alphaChannels;
+    alphaChannels.push_back(bgra[3]);
+    alphaChannels.push_back(bgra[3]);
+    alphaChannels.push_back(bgra[3]);
+    Mat alpha = Mat::zeros(foreGroundImage.size(), CV_8UC3);
+    merge(alphaChannels, alpha);
+
+    // Read background image
+    Mat background = imread("backGroundLarge.jpg");
+
+    // Convert Mat to float data type
+    foreground.convertTo(foreground, CV_32FC3);
+    background.convertTo(background, CV_32FC3);
+    alpha.convertTo(alpha, CV_32FC3, 1.0 / 255); // keeps the alpha values between 0 and 1
+
+    // Number of iterations to average the preformance over
+    int numOfIterations = 1;
+
+    // Alpha blending using functions multiply and add
+    Mat outImage = Mat::zeros(foreground.size(), foreground.type());
+    double t = (double) getTickCount();
+    for (int i = 0; i < numOfIterations; ++i) {
+        outImage = blend(alpha, foreground, background, outImage);
+    }
+    t = ((double) getTickCount() - t) / getTickFrequency();
+    cout << "Time for alpha blending using multiply & add function: "
+         << t * 1000 / numOfIterations << " milliseconds" << endl;
+
+    // Alpha blending using direct Maat access with for loop 
+    outImage = Mat::zeros(foreground.size(), foreground.type());
+    t = (double) getTickCount();
+    for (int i = 0; i < numOfIterations; ++i) {
+        outImage = alphaBlendDirectAccess(alpha, foreground, background, outImage);
+    }
+    t = ((double) getTickCount() - t) / getTickFrequency();
+    cout << "Time for alpha blending using alphaBlendDirectAccess: "
+         << t * 1000 / numOfIterations << " milliseconds" << endl;
+
+    imshow("alpha blended image", outImage / 255);
+    waitKey();
+
+    return 0;
+}
---
+++ spmallick-learnopencv/README.md
@@ -0,0 +1,5 @@
+spmallick's learnopencv
+=======================
+
+github : https://github.com/spmallick/learnopencv
+website: https://www.learnopencv.com/
