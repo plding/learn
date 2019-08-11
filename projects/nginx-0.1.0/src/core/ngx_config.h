8294a84fe54d862979627744605f9b72cee5841d
plding <77676182@qq.com>
2019-08-11 18:48:26 +0800

    update

--- projects/nginx-0.1.0/src/core/ngx_config.h
+++ projects/nginx-0.1.0/src/core/ngx_config.h
@@ -7,4 +7,34 @@
 #ifndef _NGX_CONFIG_H_INCLUDED_
 #define _NGX_CONFIG_H_INCLUDED_
 
+
+#if defined __DragonFly__ && !defined __FreeBSD__
+#define __FreeBSD__        4
+#define __FreeBSD_version  480101
+#endif
+
+
+#if defined __FreeBSD__
+#include <ngx_freebsd_config.h>
+
+
+#elif defined __linux__
+#include <ngx_linux_config.h>
+
+
+/* Solaris */
+#elif defined sun && (defined __svr4__ || defined __SVR4)
+#include <ngx_solaris_config.h>
+
+
+#elif defined _WIN32
+#include <ngx_win32_config.h>
+
+
+#else /* posix */
+#include <ngx_posix_config.h>
+
+#endif
+
+
 #endif /* _NGX_CONFIG_H_INCLUDED_ */
---
+++ projects/nginx-0.1.0/src/os/unix/ngx_posix_config.h
@@ -0,0 +1,41 @@
+
+/*
+ * Copyright (C) Ding Peilong
+ */
+
+
+#ifndef _NGX_POSIX_CONFIG_H_INCLUDED_
+#define _NGX_POSIX_CONFIG_H_INCLUDED_
+
+
+#include <sys/types.h>
+#include <sys/time.h>
+#include <unistd.h>
+#include <stdarg.h>
+#include <stddef.h>             /* offsetof() */
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+#include <signal.h>
+#include <pwd.h>
+#include <grp.h>
+#include <dirent.h>
+
+#include <sys/uio.h>
+#include <sys/filio.h>          /* FIONBIO */
+#include <sys/stat.h>
+#include <fcntl.h>
+
+#include <sys/wait.h>
+#include <sys/mman.h>
+#include <sys/resource.h>
+#include <sched.h>
+
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+
+
+#endif /* _NGX_POSIX_CONFIG_H_INCLUDED_ */
