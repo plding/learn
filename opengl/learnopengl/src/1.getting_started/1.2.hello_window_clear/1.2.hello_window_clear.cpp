5ad07f4b7c3a0a5504eaab871d0a83d6878b4d60
plding <77676182@qq.com>
2019-08-17 23:29:23 +0800

    update

--- opengl/learnopengl/src/1.getting_started/1.2.hello_window_clear/1.2.hello_window_clear.cpp
+++ opengl/learnopengl/src/1.getting_started/1.2.hello_window_clear/1.2.hello_window_clear.cpp
@@ -48,12 +48,17 @@ int main()
         // -----
         processInput(window);
 
+        // render
+        // ------
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
         // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
         // -------------------------------------------------------------------------------
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
-    
+
     // glfw: terminate, clearing all previously allocated GLFW resources.
     // ------------------------------------------------------------------
     glfwTerminate();
