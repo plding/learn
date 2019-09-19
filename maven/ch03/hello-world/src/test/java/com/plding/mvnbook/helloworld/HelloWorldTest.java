f9feaf9902ae64b45d1e1790acfa91b9c84334c7
plding <77676182@qq.com>
2019-09-19 15:17:32 +0800

    update

---
+++ maven/ch03/hello-world/src/test/java/com/plding/mvnbook/helloworld/HelloWorldTest.java
@@ -0,0 +1,14 @@
+package com.plding.mvnbook.helloworld;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class HelloWorldTest {
+
+    @Test
+    public void testSayHello() {
+        HelloWorld helloWorld = new HelloWorld();
+        String result = helloWorld.sayHello();
+        assertEquals("Hello Maven", result);
+    }
+}
