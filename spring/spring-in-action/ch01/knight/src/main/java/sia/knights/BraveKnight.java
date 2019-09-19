fa8cdb4149f848835ee82f6837d096de6fd83e16
plding <77676182@qq.com>
2019-09-19 18:02:09 +0800

    update

---
+++ spring/spring-in-action/ch01/knight/src/main/java/sia/knights/BraveKnight.java
@@ -0,0 +1,14 @@
+package sia.knights;
+
+public class BraveKnight implements Knight {
+    
+    private Quest quest;
+
+    public BraveKnight(Quest quest) {
+        this.quest = quest;
+    }
+
+    public void embarkOnQuest() {
+        quest.embark();
+    }
+}
---
+++ spring/spring-in-action/ch01/knight/src/main/java/sia/knights/Knight.java
@@ -0,0 +1,5 @@
+package sia.knights;
+
+public interface Knight {
+    void embarkOnQuest();
+}
