138dfa21437f1e25062903638ad79754f2aab8dd
plding <77676182@qq.com>
2019-12-27 17:51:09 +0800

    update

---
+++ spring/spring-in-action/Chapter_01/knight/src/main/java/sia/knights/DamselRescuingKnight.java
@@ -0,0 +1,14 @@
+package sia.knights;
+
+public class DamselRescuingKnight implements Knight {
+    
+    private RescueDamselQuest quest;
+
+    public DamselRescuingKnight() {
+        this.quest = new RescueDamselQuest();
+    }
+
+    public void embarkOnQuest() {
+        quest.embark();
+    }
+}
