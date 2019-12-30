afe3e1ca066ff24720d6a53b019b0834d3052995
plding <77676182@qq.com>
2019-12-30 15:55:10 +0800

    update

---
+++ spring/spring-in-action/Chapter_05/Spittr/src/main/java/spittr/web/HomeController.java
@@ -0,0 +1,13 @@
+package spittr.web;
+
+import static org.springframework.web.bind.annotation.RequestMethod.*;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+    @RequestMapping(value="/", method = GET)
+    public String home() {
+        return "home";
+    }
+}
