a5100f92249c49ce54bdf7fa10e08dc338ad3fe5
plding <77676182@qq.com>
2020-01-07 16:06:41 +0800

    update

---
+++ language/java/onJava8/annotations/simplest/SimpleTest.java
@@ -0,0 +1,18 @@
+// annotations/simplest/SimpleTest.java
+// Test the "Simple" annotation
+// {java annotations.simplest.SimpleTest}
+package annotations.simplest;
+
+@Simple
+public class SimpleTest {
+    @Simple int i;
+    @Simple public SimpleTest() {}
+    @Simple public void foo() { System.out.println("SimpleTest.foo()"); }
+    @Simple public void bar(String s, int i, float f) { System.out.println("SimpleTest.bar()"); } 
+    @Simple
+    public static void main(String[] args) {
+        @Simple
+        SimpleTest st = new SimpleTest();
+        st.foo();
+    }
+}
