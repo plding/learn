9c8a886dfb4038e676ede0d403223a273597fac5
plding <77676182@qq.com>
2020-01-07 17:56:53 +0800

    update

--- kafka/the-definitive-guide/src/main/java/com/plding/learnkafka/ch03/FirstProducer.java
+++ kafka/the-definitive-guide/src/main/java/com/plding/learnkafka/ch03/FirstProducer.java
@@ -1,14 +1,14 @@
-package plding.kafka.ch03;
+package com.plding.learnkafka.ch03;
 
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.Producer;
 import org.apache.kafka.clients.producer.ProducerRecord;
 
 import java.util.Properties;
+import java.util.concurrent.ExecutionException;
 
-class FirstProducer {
+public class FirstProducer {
     public static void main(String[] args) {
-
         Properties p = new Properties();
 
         // Declare the properties of cluster and information about data key and value
@@ -16,13 +16,23 @@ class FirstProducer {
         p.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
         p.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
 
-        // Create producer and send data in format : (topic name, key, value)
+        // Create producer and send data in format: (topic name, key, value)
         Producer<String, String> pd = new KafkaProducer<>(p);
         ProducerRecord<String, String> rec = new ProducerRecord<>("firsttopic", "key", "value");
 
         // Kafka has 3 methods of sending
         // 1) Fire and forget
         pd.send(rec);
+
+        // 2) Syncronous send, wait for response object
+        try {
+            pd.send(rec).get();
+        } catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+        }
+
+        // 3) Asynchronous send, given a call back function and track success using callback
+        pd.send(rec, new MyCallback());
 
         pd.close();
     }
---
+++ kafka/the-definitive-guide/src/main/java/com/plding/learnkafka/ch03/MyCallback.java
@@ -0,0 +1,14 @@
+package com.plding.learnkafka.ch03;
+
+import org.apache.kafka.clients.producer.Callback;
+import org.apache.kafka.clients.producer.RecordMetadata;
+
+class MyCallback implements Callback {
+    public void onCompletion(RecordMetadata metadata, Exception exception) {
+        if (exception != null) {
+            exception.printStackTrace();
+        } else {
+            System.out.println("Message posted callback success");
+        }
+    }
+}
