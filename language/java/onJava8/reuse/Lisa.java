9ee501beb065b453c656050228dff666dc5bdff2
plding <77676182@qq.com>
2020-01-30 16:12:26 +0800

    update

---
+++ language/java/onJava8/reuse/Lisa.java
@@ -0,0 +1,8 @@
+// reuse/Lisa.java
+// {WillNotCompile}
+
+class Lisa extends Homer {
+    @Override void doh(Milhouse m) {
+        System.out.println("doh(Milhouse)");
+    }
+}
---
+++ language/java/onJava8/reuse/Orc.java
@@ -0,0 +1,44 @@
+// reuse/Orc.java
+// The protected keyword
+class Villain {
+    private String name;
+
+    protected void set(String nm) {
+        name = nm;
+    }
+
+    Villain(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "I'm a Villain and my name is " + name;
+    }
+}
+
+public class Orc extends Villain {
+    private int orcNumber;
+
+    public Orc(String name, int orcNumber) {
+        super(name);
+        this.orcNumber = orcNumber;
+    }
+
+    public void change(String name, int orcNumber) {
+        set(name); // Available because it's protected
+        this.orcNumber = orcNumber;
+    }
+
+    @Override
+    public String toString() {
+        return "Orc " + orcNumber + ": " + super.toString();
+    }
+
+    public static void main(String[] args) {
+        Orc orc = new Orc("Limburger", 12);
+        System.out.println(orc);
+        orc.change("Bob", 19);
+        System.out.println(orc);
+    }
+}
