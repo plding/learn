207c58bf5e1cb04609c58045be2b87a720b06b13
plding <77676182@qq.com>
2020-01-31 10:06:24 +0800

    update

---
+++ language/golang/the-go-programming-language/ch02/ftoc/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+    const freezingF, boilingF = 32.0, 212.0
+    fmt.Printf("%g°F = %g°C\n", freezingF, fToc(freezingF))
+    fmt.Printf("%g°F = %g°C\n", boilingF, fToc(boilingF))
+}
+
+func fToc(f float64) float64 {
+    return (f - 32) * 5 / 9
+}
