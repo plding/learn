b161d47ee60aeff415767b159428fe844a77661d
丁配龙 <dingpeilong@inke.cn>
2023-08-23 14:01:21 +0800

    update

--- opengl/LearnOpenGL/src/1.getting_started/1.1.hello_window/hello_window.cpp
+++ opengl/LearnOpenGL/src/1.getting_started/1.1.hello_window/hello_window.cpp
@@ -7,7 +7,7 @@ void framebuffer_size_callback(GLFWwindow *window, int width, int height);
 void processInput(GLFWwindow *window);
 
 // settings
-const unsigned int SCR_WIDTH = 800;
+const unsigned int SCR_WIDTH  = 800;
 const unsigned int SCR_HEIGHT = 600;
 
 int main()
