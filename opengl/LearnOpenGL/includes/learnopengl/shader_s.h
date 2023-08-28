adf1882adc4e34ca9e5e2f447dc5c2325e895c12
丁配龙 <dingpeilong@inke.cn>
2023-08-28 21:39:26 +0800

    updat

--- opengl/LearnOpenGL/includes/learnopengl/shader_s.h
+++ opengl/LearnOpenGL/includes/learnopengl/shader_s.h
@@ -71,6 +71,20 @@ public:
     {
         glUseProgram(ID);
     }
+    // utility uniform functions
+    // ------------------------------------------------------------------------
+    void setBool(const std::string &name, bool value) const
+    {
+        glUniform1i(glGetUniformLocation(ID, name.c_str()), value);
+    }
+    void setInt(const std::string &name, int value) const
+    {
+        glUniform1i(glGetUniformLocation(ID, name.c_str()), value);
+    }
+    void setFloat(const std::string &name, float value) const
+    {
+        glUniform1f(glGetUniformLocation(ID, name.c_str()), value);
+    }
 
 private:
     // utility function for checking shader compilation/linking errors.
